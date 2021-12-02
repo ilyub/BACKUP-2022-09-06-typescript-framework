6dc79cdb083660abb5d09269e158796a2914bd22
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-02 19:37:58 +0300

    feat(dictionary): Stricter "RawDefinition" type

--- src/facade-implementations/lang/dictionary/types/index.ts
+++ src/facade-implementations/lang/dictionary/types/index.ts
@@ -2,6 +2,7 @@ import type { Context, Word } from "@skylib/facades/dist/lang";
 import type {
   NumStr,
   ReadonlyIndexedObject,
+  ReadonlyPartialRecord,
   ReadonlyRecord
 } from "@skylib/functions/dist/types/core";
 
@@ -11,7 +12,7 @@ export type RawDefinition =
   | string
   | RawDefinitions
   | readonly [NumStr, RawDefinitions]
-  | readonly [NumStr, RawDefinitions, ReadonlyIndexedObject<NumStr>];
+  | readonly [NumStr, RawDefinitions, ReadonlyPartialRecord<Context, NumStr>];
 
 // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 export interface RawDefinitions {
