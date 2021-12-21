0755cb398bb1aef988fad156a92436230869e4b3
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-21 13:00:24 +0300

    chore(refactor)

--- jest.config.fast.js
+++ jest.config.fast.js
@@ -1,10 +1,4 @@
-const config = require("@skylib/config/src/jest-preset");
-
 module.exports = {
-  maxWorkers: 4,
-  moduleNameMapper: {
-    ...config.moduleNameMapper,
-    [/^pouchdb$/u.source]: require.resolve("pouchdb/dist/pouchdb.js")
-  },
-  preset: "@skylib/config/src"
+  ...require("./jest.config"),
+  maxWorkers: 4
 };
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 const config = require("@skylib/config/src/jest-preset");
 
 module.exports = {
-  maxWorkers: 1,
   moduleNameMapper: {
     ...config.moduleNameMapper,
     [/^pouchdb$/u.source]: require.resolve("pouchdb/dist/pouchdb.js")
