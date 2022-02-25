c6dbbab50f7290f8c6db0b249d049b8f12450633
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-02-25 22:12:57 +0300

    chore(eslint): Edited rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-  extends: [require.resolve("@skylib/config/src/eslintrc")],
-  rules: {
-    "@typescript-eslint/switch-exhaustiveness-check": "off",
-    "eslint-comments/disable-enable-pair": [
-      "error",
-      { "allowWholeFile": true }
-    ],
-    // test files
-    "max-classes-per-file": "off",
-    "prefer-object-has-own": "off",
-    "regexp/prefer-lookaround": "off",
-    // test files
-    "unicorn/no-null": "off"
-  }
+  extends: [require.resolve("@skylib/config/src/eslintrc")]
 };
