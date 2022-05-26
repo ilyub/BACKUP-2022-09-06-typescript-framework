63ee4dac3a09b6c48c4139be2aa550985a1ca1ce
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-05-26 10:37:32 +0300

    test(refactor)

--- tests/facade-implementations/reactive-storage/reflect-storage.ts
+++ tests/facade-implementations/reactive-storage/reflect-storage.ts
@@ -2,7 +2,41 @@ import { implementations } from "@";
 
 const { reflectStorage } = implementations.reactiveStorage;
 
-test("reflectStorage", () => {
+test("reducer", () => {
+  const callback = jest.fn();
+
+  const obj = reflectStorage(reflectStorage({ x: 0, y: { z: 0 } }));
+
+  const observer = reflectStorage.watch(obj, callback, value => value.x);
+
+  {
+    obj.x = 1;
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith({ x: 1, y: { z: 0 } });
+    callback.mockClear();
+  }
+
+  {
+    obj.y.z = 1;
+    expect(callback).not.toHaveBeenCalled();
+  }
+
+  {
+    reflectStorage.unwatch(obj, observer);
+    obj.x = 2;
+    obj.y.z = 2;
+    expect(callback).not.toHaveBeenCalled();
+  }
+
+  {
+    Object.freeze(obj);
+    expect(() => {
+      obj.x = 3;
+    }).toThrow("'set' on proxy: trap returned falsish for property 'x'");
+  }
+});
+
+test("watch, unwatch", () => {
   const callback = jest.fn();
 
   const obj = reflectStorage(reflectStorage({ x: 0, y: { z: 0 } }));
@@ -21,40 +55,6 @@ test("reflectStorage", () => {
     expect(callback).toHaveBeenCalledTimes(1);
     expect(callback).toHaveBeenCalledWith({ x: 1, y: { z: 1 } });
     callback.mockClear();
-  }
-
-  {
-    reflectStorage.unwatch(obj, observer);
-    obj.x = 2;
-    obj.y.z = 2;
-    expect(callback).not.toHaveBeenCalled();
-  }
-
-  {
-    Object.freeze(obj);
-    expect(() => {
-      obj.x = 3;
-    }).toThrow("'set' on proxy: trap returned falsish for property 'x'");
-  }
-});
-
-test("reflectStorage: reducer", () => {
-  const callback = jest.fn();
-
-  const obj = reflectStorage(reflectStorage({ x: 0, y: { z: 0 } }));
-
-  const observer = reflectStorage.watch(obj, callback, value => value.x);
-
-  {
-    obj.x = 1;
-    expect(callback).toHaveBeenCalledTimes(1);
-    expect(callback).toHaveBeenCalledWith({ x: 1, y: { z: 0 } });
-    callback.mockClear();
-  }
-
-  {
-    obj.y.z = 1;
-    expect(callback).not.toHaveBeenCalled();
   }
 
   {
