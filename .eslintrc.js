97de25d01e193f608b24e262cf8ab26320bfa076
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-06-18 17:11:45 +0300

    chore(refactor)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,7 @@
 module.exports = {
-  extends: [require.resolve("@skylib/config/src/eslintrc")],
-  overrides: [
-    {
-      files: ["./src/facade-implementations/*/index.ts"],
-      rules: { "@skylib/only-export-name": "off" }
-    }
-  ],
-  rules: {
-    // eslint-disable-next-line no-warning-comments -- Postponed
-    // fixme
-    "boundaries/element-types": "off"
-  }
+  extends: [
+    require.resolve("@skylib/config/src/eslintrc"),
+    "./.eslintrc.overrides",
+    "./.eslintrc.temp"
+  ]
 };
