205cc71c6579784226a7c502d68e085de90b0155
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-24 23:14:46 +0300

    chore(fix): Fix after "@skylib/config" update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,9 @@ module.exports = {
     require.resolve("@skylib/config/src/eslintrc.allow-type-assertions"),
     require.resolve("@skylib/functions/configs/eslintrc"),
     require.resolve("@skylib/facades/configs/eslintrc"),
-    "./configs/eslintrc"
-  ],
-  overrides: [
-    {
-      extends: require.resolve("@skylib/config/src/eslintrc.allow-promises"),
-      files:
-        "./{src,tests}/facade-implementations/{google,handle-promise,facebook}/**"
-    },
-    {
-      extends: require.resolve(
-        "@skylib/config/src/eslintrc.skip-only-export-check"
-      ),
-      files: "./src/facade-implementations/*/index.ts"
-    },
-    { extends: ["./.eslintrc.overrides", "./.eslintrc.temp"], files: "**" }
+    "./configs/eslintrc",
+    "./.eslintrc.overrides",
+    "./.eslintrc.rule-overrides",
+    "./.eslintrc.temp"
   ]
 };
--- .eslintrc.overrides.js
+++ .eslintrc.overrides.js
@@ -1,1 +1,15 @@
-module.exports = {};
+module.exports = {
+  overrides: [
+    {
+      extends: require.resolve("@skylib/config/src/eslintrc.allow-promises"),
+      files:
+        "./{src,tests}/facade-implementations/{google,handle-promise,facebook}/**"
+    },
+    {
+      extends: require.resolve(
+        "@skylib/config/src/eslintrc.skip-only-export-check"
+      ),
+      files: "./src/facade-implementations/*/index.ts"
+    }
+  ]
+};
