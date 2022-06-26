7f50c8deebc2e92280abf34306ee99101040cc0c
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-26 15:38:24 +0300

    chore(eslint): Edited rules

--- .eslintrc.overrides.js
+++ .eslintrc.overrides.js
@@ -4,12 +4,6 @@ module.exports = {
       extends: require.resolve("@skylib/config/src/eslintrc.allow-promises"),
       files:
         "./{src,tests}/facade-implementations/{google,handle-promise,facebook}/**"
-    },
-    {
-      extends: require.resolve(
-        "@skylib/config/src/eslintrc.skip-only-export-check"
-      ),
-      files: "./src/facade-implementations/*/index.ts"
     }
   ]
 };
--- .eslintrc.temp.js
+++ .eslintrc.temp.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line @skylib/no-restricted-syntax -- Postponed
-module.exports = { rules: { "boundaries/element-types": "off" } };
+module.exports = {};
