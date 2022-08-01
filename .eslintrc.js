04e2fff55220f049dddf788a7f40033e8225d8ff
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-08-01 21:56:27 +0300

    feat: Removed "configs"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   extends: [
     require.resolve("@skylib/config/src/eslintrc"),
-    require.resolve("@skylib/functions/configs/eslintrc"),
-    require.resolve("@skylib/facades/configs/eslintrc"),
-    "./configs/eslintrc",
     "./.eslintrc.overrides",
     "./.eslintrc.rule-overrides",
     "./.eslintrc.temp"
